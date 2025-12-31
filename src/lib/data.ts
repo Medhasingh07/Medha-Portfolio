2e3650212d31137056a154fb8b619ba66de8e6b6
Medha Singh <medhasingh1350@gmail.com>
2025-12-31 11:24:20 +0000

    add react js and tailwind css amd add nectjs in the govt scheme

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -53,7 +53,7 @@ export const projects = [
   {
     title: 'Govt. Scheme Eligibility Checker',
     description: 'A user-friendly tool to help citizens check their eligibility for various government schemes based on their profile, simplifying access to public services.',
-    tech: ['Next.js'],
+    tech: ['Next.js', 'React', 'Tailwind CSS'],
     liveUrl: '#',
     repoUrl: 'https://github.com/Medhasingh07/Sarkari-Suvidha.git',
     image: findImage('project-gov-scheme'),
